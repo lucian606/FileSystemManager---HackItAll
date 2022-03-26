dde1fb5fd5de801bba7020c14477dcc25a323a4d
Stefan Popa <andrei.popa2199@gmail.com>
2022-03-26 14:19:42 +0200

    Implement react router and add components files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import './App.css';
-import LoginPage from './components/LoginPage';
-import RegisterPage from './components/RegisterPage';
-import HomePage from './components/HomePage';
-import SubjectGeneratorPage from './components/SubjectGeneratorPage';
+// import LoginPage from './components/LoginPage';
+// import RegisterPage from './components/RegisterPage';
+// import HomePage from './components/HomePage';
+// import SubjectGeneratorPage from './components/SubjectGeneratorPage';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
+import Dashboard from './components/Dashboard';
+import DeviceDetails from './components/DeviceDetails';
+import DeviceTerminal from './components/DeviceTerminal';
+import DeviceGUI from './components/DeviceGUI';
+import Login from './components/Login';
+import Footer from './components/Footer';
+// import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
+  // return (
+  //   <BrowserRouter>
+  //     <AuthProvider>
+  //       <Routes>
+  //         <Route path='/login' element={<LoginPage/>} />
+  //         <Route path='/register' element={<RegisterPage/>} />
+  //         <Route path='/home' element={<HomePage/>} />
+  //         <Route path='/something' element={<SubjectGeneratorPage/>} />      
+  //       </Routes>
+  //     </AuthProvider>
+  //   </BrowserRouter>
+  // );
+
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path='/login' element={<LoginPage/>} />
-          <Route path='/register' element={<RegisterPage/>} />
-          <Route path='/home' element={<HomePage/>} />
-          <Route path='/something' element={<SubjectGeneratorPage/>} />      
-        </Routes>
-      </AuthProvider>
+      <h1>Header</h1>
+      <Routes>
+        <Route path='/details' element={<DeviceDetails/>} />
+        <Route path='/terminal/:id' element={<DeviceTerminal/>}/>
+        <Route path='/gui/:id' element={<DeviceGUI/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/' element={<Dashboard/>}/>
+      </Routes>
+      <h1>Footer</h1>
     </BrowserRouter>
-  );
+  )
 }
 
 export default App;
